e8804384c53825d321526a5472a35915c289e25c
propy1113 <50749341+propy1113@users.noreply.github.com>
2023-05-25 10:11:22 +0900

    Add files via upload

---
+++ springSample/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.dto.UserRequest;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+public class UserController {
+
+    /**
+     * ユーザー情報 Service
+     */
+    @Autowired
+    UserService userService;
+
+    /**
+     * ユーザー情報一覧画面を表示
+     * @param model Model
+     * @return ユーザー情報一覧画面
+     */
+    @GetMapping("/user/list")
+    public String displayList(Model model) {
+        List<User> userlist = userService.searchAll();
+        model.addAttribute("userlist", userlist);
+        return "user/list";
+    }
+
+    /**
+     * ユーザー新規登録画面を表示
+     * @param model Model
+     * @return ユーザー情報一覧画面
+     */
+    @GetMapping("/user/add")
+    public String displayAdd(Model model) {
+        model.addAttribute("userRequest", new UserRequest());
+        return "user/add";
+    }
+
+    /**
+     * ユーザー新規登録
+     * @param userRequest リクエストデータ
+     * @param model Model
+     * @return ユーザー情報一覧画面
+     */
+    @PostMapping("/user/create")
+    public String create(@ModelAttribute UserRequest userRequest, Model model) {
+        // ユーザー情報の登録
+        userService.create(userRequest);
+        return "redirect:/user/list";
+    }
+}
